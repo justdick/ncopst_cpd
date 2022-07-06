41c9670bf5be8ba7b4d5fa25adbd409a58a621f5
justdick <41244006+justdick@users.noreply.github.com>
2022-07-06 10:27:00 +0000

    update

--- resources/views/prompt_or_otp.blade.php
+++ resources/views/prompt_or_otp.blade.php
@@ -11,7 +11,7 @@
                 <h6>
                     <li>
                         @if (isset($response['data']['display_text']))
-                            {{$response['data']['display_text']}}
+                            {{$response}}
                         @else
                             {{$response}}
                         @endif
