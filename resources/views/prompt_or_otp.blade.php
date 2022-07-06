882b8aad55323147e274f15b41647ed71b0baa7c
justdick <41244006+justdick@users.noreply.github.com>
2022-07-06 19:28:12 +0000

    update

--- resources/views/prompt_or_otp.blade.php
+++ resources/views/prompt_or_otp.blade.php
@@ -10,11 +10,12 @@
             <ol>
                 <h6>
                     <li>
-                        @if (isset($response['data']['display_text']))
+                        {{-- @if (isset($response['data']['display_text']))
                             {{$response['data']['display_text']}}
                         @else
                             {{$response['data']}}
-                        @endif
+                        @endif --}}
+                        Check prompt on your phone to confirm.
                     </li>
 
                     {{-- <li>
