824a21e951edb15b3499dd4720240a8220771b8f
justdick <41244006+justdick@users.noreply.github.com>
2022-07-06 15:18:57 +0000

    update

--- app/Http/Controllers/CpdController.php
+++ app/Http/Controllers/CpdController.php
@@ -21,8 +21,13 @@ class CpdController extends Controller
             return Datatables::of($cpds)
                 ->addIndexColumn()
                 ->addColumn('action', function($row){
-                    $actionBtn = '<button data-remote="'. route('cpd.update', $row->id) . '" class="btn btn-success btn-sm attended">Attended</button>';
-                    return $actionBtn;
+                    if(isset($_GET['role']) && $_GET['role'] == 'admin'){
+                        $actionBtn = '<button type="button" class="btn btn-success btn-sm">Paid</button>';
+                        return $actionBtn;
+                    }else{
+                        $actionBtn = '<button data-remote="'. route('cpd.update', $row->id) . '" class="btn btn-success btn-sm attended">Attended</button>';
+                        return $actionBtn;
+                    }
                 })
                 ->rawColumns(['action'])
                 ->make(true);
