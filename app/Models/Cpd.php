e9d2546e9ba4ad0d5bfe27dfd8c7ec4d38932a77
justdick <41244006+justdick@users.noreply.github.com>
2022-07-31 20:20:16 +0000

    update

--- app/Models/Cpd.php
+++ app/Models/Cpd.php
@@ -13,9 +13,10 @@ class Cpd extends Model
         'name', 'phone', 'network', 'email', 'staff_id', 'region', 'district', 'circuit', 'reference', 'attended'
     ];
 
-    protected $cast = [
-        'created_at' => 'date',
-    ];
+    public function getCreatedAtFormattedAttribute()
+    {
+        return $this->created_at->format('yyyy-mm-dd H:i:s');
+    }
 
     public function payments()
     {
