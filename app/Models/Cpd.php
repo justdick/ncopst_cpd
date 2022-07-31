8ba379a5c48dc30bcac0fe4718367210eb23ef9a
justdick <41244006+justdick@users.noreply.github.com>
2022-07-31 20:12:59 +0000

    update

--- app/Models/Cpd.php
+++ app/Models/Cpd.php
@@ -13,6 +13,8 @@ class Cpd extends Model
         'name', 'phone', 'network', 'email', 'staff_id', 'region', 'district', 'circuit', 'reference', 'attended'
     ];
 
+    protected $dateFormat = 'Y-m-d H:i:s';
+
     public function payments()
     {
         return $this->hasMany(Payment::class);
