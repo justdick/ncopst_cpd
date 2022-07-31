0e4cd582ceb1f0522f321ef1989102d1cf500250
justdick <41244006+justdick@users.noreply.github.com>
2022-07-31 20:23:51 +0000

    update

--- app/Models/Cpd.php
+++ app/Models/Cpd.php
@@ -13,11 +13,6 @@ class Cpd extends Model
         'name', 'phone', 'network', 'email', 'staff_id', 'region', 'district', 'circuit', 'reference', 'attended'
     ];
 
-    public function getCreatedAtAttribute()
-    {
-        return $this->created_at->format('yyyy-mm-dd H:i:s');
-    }
-
     public function payments()
     {
         return $this->hasMany(Payment::class);
